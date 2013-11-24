db2d92432d79a60855ee3beda893577acac68d47
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-24 21:46:19 +0900

    fix

--- build.sh
+++ build.sh
@@ -16,6 +16,8 @@ function install_s3cmd {
   wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
   sudo wget -q -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
   sudo apt-get -q update && sudo apt-get -q install s3cmd
+  sed -i "s/^access_key = */access_key = $AWS_ACCESS_KEY_ID/" .s3cfg
+  sed -i "s/^secret_key = */secret_key = $AWS_SECRET_ACCESS_KEY/" .s3cfg
 }
 
 function vagrant_up {
@@ -31,6 +33,10 @@ function build_rpm {
   scp -F .vagrant.ssh.config default:~/rpm/SRPMS/*.rpm .
 }
 
+function upload_rpm {
+  s3cmd -c .s3cfg puts *.rpm s3://$S3_BUCKET/
+}
+
 function terminate {
   ssh -t -t -F .vagrant.ssh.config default 'sudo shutdown -h now'
 }
@@ -39,4 +45,6 @@ install_vagrant
 install_ec2_key
 vagrant_up
 build_rpm
+install_s3cmd
+upload_rpm
 terminate
