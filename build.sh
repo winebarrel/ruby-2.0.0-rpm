85b70d1c436a9807aa5d8abe9bce342eddc3a887
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-24 19:26:36 +0900

    add build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -q $VAGRANT_URL
+sudo dpkg -i `basename $VAGRANT_URL`
+vagrant plugin install vagrant-aws
+vagrant box add dummy $DUMMY_BOX_URL
+echo "$EC2_PRIVATE_KEY" > $EC2_KEY_NAME
+chmod 600 $EC2_KEY_NAME
+vagrant up --provider=aws || echo
