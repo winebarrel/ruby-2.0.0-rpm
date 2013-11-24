92144428be495f0af0e14229d0da56bfc591a678
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-24 20:05:53 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
-wget -q $VAGRANT_URL
-sudo dpkg -i `basename $VAGRANT_URL`
-vagrant plugin install vagrant-aws
-vagrant box add dummy $DUMMY_BOX_URL
+function install_vagrant {
+  wget -q $VAGRANT_URL
+  sudo dpkg -i `basename $VAGRANT_URL`
+  vagrant plugin install vagrant-aws
+  vagrant box add dummy $DUMMY_BOX_URL
+}
 
-echo "$EC2_PRIVATE_KEY" > $EC2_KEY_NAME
-sed -i 's/^export *//' $EC2_KEY_NAME
-chmod 600 $EC2_KEY_NAME
+function install_ec2_key {
+  echo "$EC2_PRIVATE_KEY" > $EC2_KEY_NAME
+  sed -i 's/^export *//' $EC2_KEY_NAME
+  chmod 600 $EC2_KEY_NAME
+}
 
-wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
-sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
-sudo apt-get update && sudo apt-get install s3cmd
+function install_s3cmd {
+  wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
+  sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+  sudo apt-get update && sudo apt-get install s3cmd
+}
 
-vagrant up --provider=aws 2> /dev/null || true
-vagrant ssh -c 'sed -i /requiretty/d /etc/sudoers' || true
-vagrant ssh-config > .vagrant.ssh.config
-scp -F .vagrant.ssh.config build-rpm.sh ruby200.spec default:~/
-vagrant ssh -c '/bin/bash ~/build-rpm.sh'
-vagrant destroy
+function vagrant_up {
+  vagrant up --provider=aws 2> /dev/null || true
+  vagrant ssh -c 'sudo sed -i /requiretty/d /etc/sudoers' || true
+  vagrant ssh-config > .vagrant.ssh.config
+}
+
+function build_rpm {
+  scp -F .vagrant.ssh.config build-rpm.sh ruby200.spec default:~/
+  vagrant ssh -c '/bin/bash ~/build-rpm.sh'
+}
+
+function vagrant_destroy {
+  vagrant destroy
+}
+
+install_vagrant
+install_ec2_key
+vagrant_up
+build_rpm
+vagrant_destroy
