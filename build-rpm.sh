cfd7b161b8dc784091e6ff8fbeab5ec462e53a8a
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-24 19:53:44 +0900

    fix

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo test
+sudo yum install -y rpm-build yum-utils
+mkdir -p ~/rpm/{BUILD,SRPMS,SPECS,SOURCES,RPMS}
+echo "%_topdir $HOME/rpm" > $HOME/.rpmmacros
+mv ~/ruby200.spec ~/rpm/SPECS
--- build.sh
+++ build.sh
@@ -3,11 +3,17 @@ wget -q $VAGRANT_URL
 sudo dpkg -i `basename $VAGRANT_URL`
 vagrant plugin install vagrant-aws
 vagrant box add dummy $DUMMY_BOX_URL
+
 echo "$EC2_PRIVATE_KEY" > $EC2_KEY_NAME
 sed -i 's/^export *//' $EC2_KEY_NAME
 chmod 600 $EC2_KEY_NAME
+
+wget -O- -q http://s3tools.org/repo/deb-all/stable/s3tools.key | sudo apt-key add -
+sudo wget -O/etc/apt/sources.list.d/s3tools.list http://s3tools.org/repo/deb-all/stable/s3tools.list
+sudo apt-get update && sudo apt-get install s3cmd
+
 vagrant up --provider=aws 2> /dev/null || echo
 vagrant ssh-config > .vagrant.ssh.config
-scp -F .vagrant.ssh.config build-rpm.sh default:~/
+scp -F .vagrant.ssh.config build-rpm.sh ruby200.spec default:~/
 vagrant ssh -c '/bin/bash ~/build-rpm.sh'
 vagrant destroy
