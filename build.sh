d7e85da89d469eb0d5ae9f1c0bceee985d455e1d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2013-11-24 19:36:20 +0900

    fix

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ sudo dpkg -i `basename $VAGRANT_URL`
 vagrant plugin install vagrant-aws
 vagrant box add dummy $DUMMY_BOX_URL
 echo "$EC2_PRIVATE_KEY" > $EC2_KEY_NAME
+sed -i 's/^export *//' $EC2_KEY_NAME
 chmod 600 $EC2_KEY_NAME
-tail -n 1 $EC2_KEY_NAME
 vagrant up --provider=aws || echo
